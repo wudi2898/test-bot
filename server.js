17a177c3520984bdb2df325cd9a0f98e707531d3
tm <tm@gmail.com>
2025-09-05 09:35:20 +0700

    1

--- server.js
+++ server.js
@@ -59,84 +59,88 @@ import { decodeBase64 } from "./utils/tool.js";
 
 // 基础路由
 app.get("/", async (req, res) => {
-  const tgWebAppStartParam = decodeBase64(req.query.tgWebAppStartParam);
-  console.log("/ tgWebAppStartParam", tgWebAppStartParam[2]);
-  // 这里会获取参数 会根据参数返回格式化后的页面
-  const lang = req.query.lang || "en";
-  const price = tgWebAppStartParam[1];
-  const name = tgWebAppStartParam[2];
-  const timestamp = tgWebAppStartParam[5];
-  const dnsRes = await fetch(`${process.env.TONAPI_URL}/v2/dns/${name}.t.me`);
-  const dnsData = await dnsRes.json();
-  // console.log("dnsData", JSON.stringify(dnsData, null, 2));
-  const nftItemAddr = dnsData?.item?.address;
-  if (!nftItemAddr) throw new Error("未找到该用户名的 NFT");
-  const historyRes = await fetch(
-    `${process.env.TONAPI_URL}/v2/nfts/${nftItemAddr}/history?limit=1000`
-  );
-  const historyJson = await historyRes.json();
-  const result = historyJson.events
-    // .filter((event) => event.actions[0].type == "NftItemTransfer")
-    .map((event) => {
-      return {
-        event_id: event.event_id,
-        // 旧持有人
-        address: new TonWeb.utils.Address(event.account.address).toString(
-          true,
-          true,
-          true
-        ),
-        // 新持有人
-        newOwner: new TonWeb.utils.Address(
-          event.actions[0].NftItemTransfer.recipient.address
-        ).toString(true, true, true),
-        // 时间戳
-        timestamp: event.timestamp,
-        // 备注
-        comment: event.actions[0].NftItemTransfer.comment,
-        // NFT item 地址
-        nftItemAddr: new TonWeb.utils.Address(nftItemAddr).toString(
-          true,
-          true,
-          true
-        ),
-      };
-    });
-  // const transactionsRes = await fetch(
-  //   `${process.env.TONAPI_URL}/v2/blockchain/accounts/${nftItemAddr}/transactions?sort_order=desc`
-  // );
-  // const transactionsJson = await transactionsRes.json();
-  // const result2 = transactionsJson.transactions
-  //   // .filter((transaction) => transaction.in_msg.source?.is_wallet)
-  //   .map((transaction) => {
-  //     return {
-  //       hash: transaction.hash,
-  //       // 发起人地址
-  //       from: transaction?.in_msg?.source?.address
-  //         ? new TonWeb.utils.Address(
-  //             transaction?.in_msg?.source?.address
-  //           ).toString(true, true, true)
-  //         : null,
-  //       // 接收人地址
-  //       to: new TonWeb.utils.Address(
-  //         transaction.in_msg.destination.address
-  //       ).toString(true, true, true),
-  //       // 转账金额
-  //       amount: transaction.in_msg.value.toString(),
-  //       // 操作码
-  //       opCode: transaction.in_msg.op_code?.toString(16),
-  //       // 时间戳
-  //       timestamp: transaction.utime * 1000,
-  //     };
-  //   });
+  if (req.query?.tgWebAppStartParam) {
+    const tgWebAppStartParam = decodeBase64(req.query?.tgWebAppStartParam);
+    console.log("/ tgWebAppStartParam", tgWebAppStartParam[2]);
+    // 这里会获取参数 会根据参数返回格式化后的页面
+    const lang = req.query.lang || "en";
+    const price = tgWebAppStartParam[1];
+    const name = tgWebAppStartParam[2];
+    const timestamp = tgWebAppStartParam[5];
+    const dnsRes = await fetch(`${process.env.TONAPI_URL}/v2/dns/${name}.t.me`);
+    const dnsData = await dnsRes.json();
+    // console.log("dnsData", JSON.stringify(dnsData, null, 2));
+    const nftItemAddr = dnsData?.item?.address;
+    if (!nftItemAddr) throw new Error("未找到该用户名的 NFT");
+    const historyRes = await fetch(
+      `${process.env.TONAPI_URL}/v2/nfts/${nftItemAddr}/history?limit=1000`
+    );
+    const historyJson = await historyRes.json();
+    const result = historyJson.events
+      // .filter((event) => event.actions[0].type == "NftItemTransfer")
+      .map((event) => {
+        return {
+          event_id: event.event_id,
+          // 旧持有人
+          address: new TonWeb.utils.Address(event.account.address).toString(
+            true,
+            true,
+            true
+          ),
+          // 新持有人
+          newOwner: new TonWeb.utils.Address(
+            event.actions[0].NftItemTransfer.recipient.address
+          ).toString(true, true, true),
+          // 时间戳
+          timestamp: event.timestamp,
+          // 备注
+          comment: event.actions[0].NftItemTransfer.comment,
+          // NFT item 地址
+          nftItemAddr: new TonWeb.utils.Address(nftItemAddr).toString(
+            true,
+            true,
+            true
+          ),
+        };
+      });
+    // const transactionsRes = await fetch(
+    //   `${process.env.TONAPI_URL}/v2/blockchain/accounts/${nftItemAddr}/transactions?sort_order=desc`
+    // );
+    // const transactionsJson = await transactionsRes.json();
+    // const result2 = transactionsJson.transactions
+    //   // .filter((transaction) => transaction.in_msg.source?.is_wallet)
+    //   .map((transaction) => {
+    //     return {
+    //       hash: transaction.hash,
+    //       // 发起人地址
+    //       from: transaction?.in_msg?.source?.address
+    //         ? new TonWeb.utils.Address(
+    //             transaction?.in_msg?.source?.address
+    //           ).toString(true, true, true)
+    //         : null,
+    //       // 接收人地址
+    //       to: new TonWeb.utils.Address(
+    //         transaction.in_msg.destination.address
+    //       ).toString(true, true, true),
+    //       // 转账金额
+    //       amount: transaction.in_msg.value.toString(),
+    //       // 操作码
+    //       opCode: transaction.in_msg.op_code?.toString(16),
+    //       // 时间戳
+    //       timestamp: transaction.utime * 1000,
+    //     };
+    //   });
 
-  console.log("=========================");
-  // console.log("transactionsJson", JSON.stringify(transactionsJson, null, 2));
-  // console.log("result2", result2);
-  // console.log("historyJson", JSON.stringify(historyJson, null, 2));
-  console.log("result", result);
-  console.log("=========================");
-  res.render(`${lang}/index`, { name, price, timestamp });
+    console.log("=========================");
+    // console.log("transactionsJson", JSON.stringify(transactionsJson, null, 2));
+    // console.log("result2", result2);
+    // console.log("historyJson", JSON.stringify(historyJson, null, 2));
+    console.log("result", result);
+    console.log("=========================");
+    res.render(`${lang}/index`, { name, price, timestamp });
+  } else {
+    res.status(401);
+  }
 });
 
 // API路由
